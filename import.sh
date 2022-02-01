d472be769cb74134e8ed2959308bbbca56ba60f9
thomas-v <thomas.vitiello@outlook.fr>
2022-02-01 23:42:58 +0100

    ajout de variable

--- import.sh
+++ import.sh
@@ -1,4 +1,7 @@
-wget -O temp/$(date '+%Y%m%d') https://donnees.roulez-eco.fr/opendata/jour
-unzip -o temp/$(date '+%Y%m%d')
-rm -f temp/$(date '+%Y%m%d')
-mv PrixCarburants_quotidien_20220131.xml temp/$(date '+%Y%m%d').xml
+#!/bin/bash
+file=$(date '+%Y%m%d')
+
+wget -O temp/$file https://donnees.roulez-eco.fr/opendata/jour
+unzip -o temp/$file
+rm -f temp/$file
+mv PrixCarburants_quotidien_20220131.xml temp/$file.xml
