299879f195fda2d81208c396adb31a8539e9f29f
thomas-v <thomas.vitiello@outlook.fr>
2022-02-01 23:40:15 +0100

    extract zip dir

--- import.sh
+++ import.sh
@@ -1,1 +1,4 @@
-wget -O temp/$(date '+%Y-%m-%d') https://donnees.roulez-eco.fr/opendata/jour
+wget -O temp/$(date '+%Y%m%d') https://donnees.roulez-eco.fr/opendata/jour
+unzip -o temp/$(date '+%Y%m%d')
+rm -f temp/$(date '+%Y%m%d')
+mv PrixCarburants_quotidien_20220131.xml temp/$(date '+%Y%m%d').xml
