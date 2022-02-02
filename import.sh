d1da330a96b741bb2e20cd8315b603713de08df9
thomas-v <thomas.vitiello@outlook.fr>
2022-02-02 12:49:58 +0100

    amelioration script import

--- import.sh
+++ import.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 file=$(date '+%Y%m%d')
+tempDir="temp/"
+url="https://donnees.roulez-eco.fr/opendata/jour"
 
-wget -O temp/$file https://donnees.roulez-eco.fr/opendata/jour
-unzip -o temp/$file
-rm -f temp/$file
-mv PrixCarburants_quotidien_20220131.xml temp/$file.xml
+wget -O $tempDir/$file $url
+unzip -o -d $tempDir/ $tempDir/$file
+rm -f $tempDir/$file
+mv $tempDir/PrixCarburants_quotidien_$(date --date='1 days ago' +%Y%m%d).xml $tempDir/$file.xml
