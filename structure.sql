fd57c5f85be2c3e731ffe6843e70da2e7428aa53
thomas-v <thomas.vitiello@outlook.fr>
2022-02-02 13:32:41 +0100

    bdd structure

---
+++ structure.sql
@@ -0,0 +1,17 @@
+CREATE TABLE IF NOT EXISTS `pdv_liste` (
+  `pdv` int(11) NOT NULL,
+  `coords` point NOT NULL,
+  `adresse` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`pdv`),
+  SPATIAL KEY `SPATIAL` (`coords`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `carburants` (
+  `id` int(11) NOT NULL,
+  `pdv` int(11) NOT NULL,
+  `type` varchar(255) NOT NULL,
+  `prix` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `pdv` (`pdv`),
+  CONSTRAINT `carburants_ibfk_1` FOREIGN KEY (`pdv`) REFERENCES `pdv_liste` (`pdv`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
