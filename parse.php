1ee8e2fc053f486d69a43d02739b5bf0783f3b7b
thomas-v <thomas.vitiello@outlook.fr>
2022-02-03 13:07:18 +0100

    Exception si fichier n existe pas

--- parse.php
+++ parse.php
@@ -1,9 +1,14 @@
 <?php
-$stream = fopen('temp/'.date('Ymd').'.xml', 'r');
-
 $file = 'temp/'.date('Ymd').'.xml';
-if (file_exists($file)) {
-    $xml = simplexml_load_file($file);
-} else {
-    exit('Echec lors de l\'ouverture du fichier test.xml.');
+try{
+    if (file_exists($file)) {
+        $pdv_liste = simplexml_load_file($file);
+        foreach($pdv_liste as $pdv){
+        }
+    } else {
+        throw new Exception("Fichier non trouvé ($file)");
+    }
+} catch(Exception $e){
+    die('Erreur : '.$e->getMessage());
 }
+ 
