be85d84b4420cc9f7219dbf230b003a1183c4214
thomas-v <thomas.vitiello@outlook.fr>
2022-02-03 13:26:34 +0100

    Connexion class pdo

---
+++ Connexion.php
@@ -0,0 +1,33 @@
+<?php
+class Connexion {
+
+	public function __construct(
+        private string $host,
+        private string $login,
+        private string $password,
+        private string $db,
+        private ?PDO $co,
+    ){
+		$this->conn();
+	}
+
+	private function conn(){
+		try
+		{
+	         $bdd = new PDO('mysql:host='.$this->host.';dbname='.$this->db.';charset=utf8mb4', $this->login, $this->password);
+			$bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+			$bdd->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+			$this->co = $bdd;
+		}
+		catch (PDOException $e)
+		{
+			die('ERREUR PDO : ' . $e->getMessage());
+		}
+	}
+
+    public function getCo():PDO{
+        return $this->co;
+    }
+
+
+}
