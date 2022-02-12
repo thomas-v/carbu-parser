89e188cca49fb6ac6ba81de5fb9e6060269abbb5
thomas-v <thomas.vitiello@outlook.fr>
2022-02-12 22:47:31 +0100

    insertions des point de ventes

--- parse.php
+++ parse.php
@@ -8,6 +8,16 @@ try{
     if (file_exists($file)) {
         $pdv_liste = simplexml_load_file($file);
         foreach($pdv_liste as $pdv){
+            $sql = 'INSERT INTO pdv_liste VALUES (:pdv, :latitude, :longitude, :adresse, :ville, :cp)';
+            $params = [
+                ':pdv'       => $pdv->attributes()['id'],
+                ':latitude' => $pdv->attributes()['latitude']/100000,
+                ':longitude'  => $pdv->attributes()['longitude']/100000,
+                ':adresse'  => $pdv->adresse,
+                ':ville' => $pdv->ville,
+                ':cp' => $pdv->attributes()['cp']
+            ];
+            $db->query($sql, $params);
         }
     } else {
         throw new Exception("Fichier non trouvé ($file)");
