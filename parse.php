91bf5970f5ed56083fe88f7c3acab4509e4c81d3
thomas-v <thomas.vitiello@outlook.fr>
2022-02-26 14:30:33 +0100

    insert carburants

--- parse.php
+++ parse.php
@@ -4,26 +4,44 @@ require_once 'env.php';
 $db = new Connexion($host, $databaseName, $password, $user, null);
 
 $file = 'temp/'.date('Ymd').'.xml';
-try{
-    if (file_exists($file)) {
-        $pdv_liste = simplexml_load_file($file);
-        foreach($pdv_liste as $pdv){
-            $sql = 'INSERT INTO pdv_liste VALUES (:pdv, :latitude, :longitude, :adresse, :ville, :cp) 
-                    ON DUPLICATE KEY UPDATE latitude=:latitude, longitude=:longitude, adresse=:adresse, ville=:ville, cp=:cp';
+
+if (file_exists($file)) {
+    $pdv_liste = simplexml_load_file($file);
+    foreach($pdv_liste as $pdv){
+        $sql = 'INSERT INTO pdv_liste VALUES (:pdv, :latitude, :longitude, :adresse, :ville, :cp) 
+                ON DUPLICATE KEY UPDATE latitude=:latitude, longitude=:longitude, adresse=:adresse, ville=:ville, cp=:cp';
+        $params = [
+            ':pdv'          => $pdv->attributes()['id'],
+            ':latitude'     => $pdv->attributes()['latitude']/100000,
+            ':longitude'    => $pdv->attributes()['longitude']/100000,
+            ':adresse'      => $pdv->adresse,
+            ':ville'        => $pdv->ville,
+            ':cp'           => $pdv->attributes()['cp']
+        ];
+        $db->query($sql, $params);
+        foreach($pdv->prix as $carb){
+            $sql = 'SELECT 1 FROM carburants WHERE pdv=:pdv AND type=:nom';
             $params = [
-                ':pdv'       => $pdv->attributes()['id'],
-                ':latitude' => $pdv->attributes()['latitude']/100000,
-                ':longitude'  => $pdv->attributes()['longitude']/100000,
-                ':adresse'  => $pdv->adresse,
-                ':ville' => $pdv->ville,
-                ':cp' => $pdv->attributes()['cp']
+                ':pdv'  => $pdv->attributes()['id'],
+                ':nom'  => $carb->attributes()['nom'],
+            ];
+            $stmt = $db->query($sql, $params);
+            if($carbu = $stmt->fetch()){
+                $sql = 'UPDATE carburants SET prix = :prix, maj = :maj WHERE pdv=:pdv AND type=:type';
+
+            } else {
+                $sql = 'INSERT INTO carburants (pdv, type, prix, maj) VALUES (:pdv, :type, :prix, :maj)';
+            }
+            $params = [
+                ':pdv'      => $pdv->attributes()['id'],
+                ':type'     => $carb->attributes()['nom'],
+                ':prix'     => $carb->attributes()['valeur'],
+                ':maj'      => $carb->attributes()['maj'],
             ];
             $db->query($sql, $params);
         }
-    } else {
-        throw new Exception("Fichier non trouvé ($file)");
     }
-} catch(Exception $e){
-    die('Erreur : '.$e->getMessage());
+} else {
+    die("Fichier non trouvé ($file)");
 }
  
