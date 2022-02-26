ccae26e31d12ce0943d7717687147333642ab491
thomas-v <thomas.vitiello@outlook.fr>
2022-02-26 14:31:13 +0100

    ajout auto increment table carburants

--- structure.sql
+++ structure.sql
@@ -9,12 +9,12 @@ CREATE TABLE IF NOT EXISTS `pdv_liste` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE `carburants` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `pdv` int(11) NOT NULL,
   `type` varchar(255) NOT NULL,
   `prix` int(11) DEFAULT NULL,
   `maj` date NOT NULL DEFAULT '0000-00-00',
-  PRIMARY KEY (`id`),
+  PRIMARY KEY (`id`) AUTO INCREMENT,
   KEY `pdv` (`pdv`),
   CONSTRAINT `carburants_ibfk_1` FOREIGN KEY (`pdv`) REFERENCES `pdv_liste` (`pdv`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
