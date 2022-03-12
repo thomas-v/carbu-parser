7894a6757aa2de741e3e16349bc33de258d2ce66
thomas-v <thomas.vitiello@outlook.fr>
2022-03-12 21:10:33 +0100

    exec php script

--- import.sh
+++ import.sh
@@ -7,3 +7,5 @@ wget -O $tempDir/$file $url
 unzip -o -d $tempDir/ $tempDir/$file
 rm -f $tempDir/$file
 mv $tempDir/PrixCarburants_quotidien_$(date --date='1 days ago' +%Y%m%d).xml $tempDir/$file.xml
+
+php parse.php
